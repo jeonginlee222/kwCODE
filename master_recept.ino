6ca5c07b8235bcbe70cc743d37e0a5be87e3769d
letsgetit16 <rezzo009@naver.com>
2024-05-28 18:06:29 +0900

    Create master_recept.ino

---
+++ master_recept.ino
@@ -0,0 +1,21 @@
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial BTSerial(2, 3); // RX, TX
+
+void setup() {
+  Serial.begin(9600);  // 시리얼 모니터와의 통신 속도
+  BTSerial.begin(9600); // HC-05 모듈과의 통신 속도
+
+  Serial.println("Master ready. Enter text to send to slave:");
+}
+
+void loop() {
+  // 시리얼 모니터에서 입력 받은 데이터를 HC-05로 전송
+  if (Serial.available()) {
+    while (Serial.available()) {
+      char c = Serial.read();
+      BTSerial.write(c);
+    }
+  }
+}
